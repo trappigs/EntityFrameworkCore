43bd581667e427419f9b5a2d0520cdf77b065266
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-02-19 22:41:29 +0300

    Kayıt Ekleme/66
    Ogrenciler tablosuna ogrenci eklemek için bir create viewi yazdık. Buna uygun şekilde veri tabanına kayıt eklemek için OgrenciController'a kod yazdık.

---
+++ EntityFrameworkCore/Controllers/OgrenciController.cs
@@ -0,0 +1,35 @@
+﻿using EntityFrameworkCore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCore.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // datacontext sınıfından bir nesne oluşturuluyor ve bu nesne ile veritabanı işlemleri yapılıyor
+        private readonly DataContext _context;
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            // _context üzerinden Ogrenciler tablosuna model nesnesi ekleniyor
+            _context.Ogrenciler.Add(model);
+
+            // değişiklikler veritabanına kaydediliyor
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index","Home");
+        }
+
+
+    }
+}
