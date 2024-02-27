0519a0cafa2b7796c1a626c5e4e7c973a8f6659d
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-02-27 09:59:06 +0300

    Kurs Kaydının Eklenmesi/75

--- EntityFrameworkCore/Data/Ogrenci.cs
+++ EntityFrameworkCore/Data/Ogrenci.cs
@@ -7,9 +7,15 @@ namespace EntityFrameworkCore.Data
         // id => primary key
         [Key]
         public int OgrenciId { get; set; }
-        
+
         public string? OgrenciAd { get; set; }
         public string? OgrenciSoyad { get; set; }
+
+        public string AdSoyad
+        {
+            get
+            { return this.OgrenciAd + " " + this.OgrenciSoyad; }
+        }
 
         public string? Eposta { get; set; }
         public string? Telefon { get; set; }
